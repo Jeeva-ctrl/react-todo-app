c33357fd7348f1d359c62a5f21dd9659a9a31a9f
Jeeva-ctrl <jeevabharathy66@gmail.com>
2020-06-11 16:36:14 +1000

    Updates

---
+++ precache-manifest.946bdca16dcc85374b32c919542215b8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388069e805e40ff8bc4e9fca3be0ca4f",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "4b13b214d96f1a36b1b1",
+    "url": "/react-todo-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "480aec8fddcf99c04d1c",
+    "url": "/react-todo-app/static/css/main.f11fe73c.chunk.css"
+  },
+  {
+    "revision": "4b13b214d96f1a36b1b1",
+    "url": "/react-todo-app/static/js/2.4c3a4bcc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-app/static/js/2.4c3a4bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "480aec8fddcf99c04d1c",
+    "url": "/react-todo-app/static/js/main.afd311ff.chunk.js"
+  },
+  {
+    "revision": "6a9141e159c13e13843d",
+    "url": "/react-todo-app/static/js/runtime-main.674c9f60.js"
+  },
+  {
+    "revision": "17dfc335a9f8e277dc5d22f9abefe3c5",
+    "url": "/react-todo-app/static/media/cut.17dfc335.png"
+  },
+  {
+    "revision": "55a22606754d7e9394b27e5a36de3254",
+    "url": "/react-todo-app/static/media/red_tick.55a22606.png"
+  },
+  {
+    "revision": "745d61710758d5127ec3606cd16fe146",
+    "url": "/react-todo-app/static/media/tick_icon.745d6171.png"
+  }
+]);
